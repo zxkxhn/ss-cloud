09d1fbae19dd6571e229898a2523d9743761ad80
zhangxk <980137428@qq.com>
2020-04-22 18:09:03 +0800

    成功启动

--- ss-databases/src/main/java/com/ss/databases/config/MybatisPlusConfig.java
+++ ss-databases/src/main/java/com/ss/databases/config/MybatisPlusConfig.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableTransactionManagement
 @Configuration
+@MapperScan("com.*.*.biz.dao")
 public class MybatisPlusConfig {
 
     @Bean
